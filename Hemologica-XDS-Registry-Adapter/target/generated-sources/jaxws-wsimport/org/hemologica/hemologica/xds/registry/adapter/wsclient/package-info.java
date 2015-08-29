a10472687d60ba03457b570402560214d2e6a8da
paularoche <pula14@gmail.com>
2015-08-29 18:11:23 -0300

    remove target files

--- Hemologica-XDS-Registry-Adapter/target/generated-sources/jaxws-wsimport/org/hemologica/hemologica/xds/registry/adapter/wsclient/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * IHE XDS.b Document Registry
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:hl7-org:v3", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.hemologica.hemologica.xds.registry.adapter.wsclient;
