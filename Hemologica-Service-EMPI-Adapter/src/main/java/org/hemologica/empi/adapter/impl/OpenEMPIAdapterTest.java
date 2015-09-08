320c7e990c96614b84f20f8f0163e18ccd0a0315
bruno <brunostrasser18@gmail.com>
2015-09-08 01:50:48 -0300

    Add OpenEMPIAdapterTest

---
+++ Hemologica-Service-EMPI-Adapter/src/main/java/org/hemologica/empi/adapter/impl/OpenEMPIAdapterTest.java
@@ -0,0 +1,34 @@
+package org.hemologica.empi.adapter.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OpenEMPIAdapterTest {
+
+	@Test
+	public void testOpenEMPIAdapter() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreate() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMerge() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQuery() {
+//		fail("Not yet implemented");
+	}
+
+}
