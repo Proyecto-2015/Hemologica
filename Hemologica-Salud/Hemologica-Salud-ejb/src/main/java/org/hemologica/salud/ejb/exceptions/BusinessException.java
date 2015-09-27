a544a0e873455c073a040113b75d35d595f8b697
bruno <brunostrasser18@gmail.com>
2015-09-27 18:04:58 -0300

    Hemologica-Salud-web donations

---
+++ Hemologica-Salud/Hemologica-Salud-ejb/src/main/java/org/hemologica/salud/ejb/exceptions/BusinessException.java
@@ -0,0 +1,10 @@
+package org.hemologica.salud.ejb.exceptions;
+
+public class BusinessException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8006677959457452875L;
+
+}
