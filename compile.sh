21b41202a9643afa9db04c89d3ed1a0814b0670b
bruno <brunostrasser18@gmail.com>
2015-11-01 10:44:51 -0300

    Paula en crisis

---
+++ compile.sh
@@ -0,0 +1,24 @@
+cd Hemologica-Datatypes
+mvn install
+cd ..
+echo "Build: Hemologica-DataTypes"
+cd Hemologica-DAO
+mvn install
+cd ..
+echo "Build: Hemologica-DAO"
+cd Hemologica-Yodono-Backend
+mvn install
+cd ..
+echo "Build: Hemologica-Yodono-Backend"
+cd Hemologica-Yodono-Frontend
+mvn install
+cd ..
+echo "Build: Hemologica-Yodono-Frontend"
+cd Hemologica-Salud-Backend
+mvn install
+cd ..
+echo "Build: Hemologica-Salud-Backend"
+cd Hemologica-Salud-Frontend
+mvn install
+cd ..
+echo "Build: Hemologica-Salud-Frontend"
