f7bad4ed1d88a994797a7befb70558012cd92414
paularoche <pula14@gmail.com>
2015-11-02 21:13:42 -0300

    data types

--- Hemologica-DataTypes/src/main/java/org/hemologica/constants/DataDonationRejectionType.java
+++ Hemologica-DataTypes/src/main/java/org/hemologica/constants/DataDonationRejectionType.java
@@ -15,16 +15,4 @@ public enum DataDonationRejectionType implements Serializable {
 			this.label = label;
 		}
 		
-//		public static List<DataDonationRejectionType> RejectionType(){
-//			List<DataDonationRejectionType> ret = new ArrayList<DataDonationRejectionType>();
-//			DataDonationRejectionType state = new DataDonationRejectionType();
-//			state.setCode(TEMPORARY.value);
-//			state.setDisplayName(TEMPORARY.label);
-//			ret.add(state);
-//			state = new DataDonationState();
-//			state.setCode(PERMANENT.value);
-//			state.setDisplayName(PERMANENT.label);
-//			ret.add(state);
-//			return ret;
-//		}
 }
