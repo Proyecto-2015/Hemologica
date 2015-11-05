d757559d6feed4f4753ec448e05e416141e62490
paularoche <pula14@gmail.com>
2015-11-04 22:14:11 -0300

    correccion enum estado donacion

--- Hemologica-DataTypes/src/main/java/org/hemologica/constants/DataDonationStateEnum.java
+++ Hemologica-DataTypes/src/main/java/org/hemologica/constants/DataDonationStateEnum.java
@@ -32,4 +32,21 @@ public enum DataDonationStateEnum implements Serializable {
 //		ret.add(state);
 		return ret;
 	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	
 }
