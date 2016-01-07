249d4bec1fa80fabd9ddd4601776cfcb66819447
paularoche <pula14@gmail.com>
2016-01-06 21:03:55 -0300

    notificaciones

--- Hemologica-Salud-Backend/Hemologica-Salud-Backend-ejb/src/main/java/org/hemologica/salud/ejb/business/NotificationThread.java
+++ Hemologica-Salud-Backend/Hemologica-Salud-Backend-ejb/src/main/java/org/hemologica/salud/ejb/business/NotificationThread.java
@@ -1,14 +1,25 @@
 package org.hemologica.salud.ejb.business;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.persistence.EntityManager;
 
 import org.hemologica.constants.Constants;
@@ -26,6 +37,7 @@ import org.hemologica.datatypes.MailData;
 import org.hemologica.factories.FactoryDAO;
 import org.hemologica.salud.ejb.beans.impl.StatisticsBean;
 import org.hemologica.salud.ejb.utils.XMLUtils;
+import org.hemologica.xmldatabase.connection.impl.BaseXConnection;
 import org.hemologica.xmldatabase.exceptions.XMLDataBaseException;
 import org.hemologica.xmldatabase.factories.XMLDataBaseFactory;
 
@@ -159,40 +171,101 @@ public class NotificationThread extends Thread{
 			 * Personas
 			 */
 			
+			Properties prop = new Properties();
+			Session session = null;
+			final String username;
+			final String password;
+			try {
+				
+				prop.load(BaseXConnection.class.getClassLoader().getResourceAsStream("hemosalud.properties"));
+				username = prop.getProperty("mail.username");
+				password = prop.getProperty("mail.password");
+				
+				if(username == null || password == null)
+					return;
+				
+				session = Session.getInstance(prop, new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(username, password);
+					}
+				});
+				
+			} catch (IOException e) {
+				
+				logger.log(Level.SEVERE, "Error al leer el archivo de propiedades hemosalud.properties", e);
+				return;
+			}
+			
 			List<NotificationsPerson> persons = new ArrayList<>();
 			
 			for(Person p : FactoryDAO.getPeronDAO(em).getPersonsFilters(new HashMap<String,Object>())){
 				
-				List<String> orClausesCDAsIds = new ArrayList<>();
-				for(PersonsRecord personRecord :FactoryDAO.getPersonRecordDAO(em).getCDAsUserId(p.getId())){
-		
-					String query = "$doc//ClinicalDocument//id/@root='"+ personRecord.getPersonsRecordCdaRoot() + "' and " +
-					"$doc//ClinicalDocument//id/@extension='" + personRecord.getPersonsRecordCdaExtension() +"'";
-					orClausesCDAsIds.add(query);
-					
-				}
-				List<List<String>> or = new ArrayList<>();
-				or.add(orClauses);
-				try {
-					
-					if((andClauses.size() == 0 && orClauses.size() ==0) ||
-							(andClauses.size() != 0 && XMLDataBaseFactory.getIXMLDataBaseDonations().countQuery(andClauses,null,orClausesCDAsIds,null) > 0
-							&& XMLDataBaseFactory.getIXMLDataBaseDonations().countQuery(null,or,orClausesCDAsIds,null) == 0) || 
-							(andClauses.size() == 0 && XMLDataBaseFactory.getIXMLDataBaseDonations().countQuery(null,or,orClausesCDAsIds,null) == 0)){
-						
-						NotificationsPerson notificationPerson = new NotificationsPerson();
-						notificationPerson.setPerson(p);
-						notificationPerson.setNotification(notification);
-						notificationPerson.setSentOption(SentOptions.NOT_SENT);
-						persons.add(notificationPerson);
+				if(p.getAllowNotificationNeedDonor()){
+				
+					List<String> orClausesCDAsIds = new ArrayList<>();
+					for(PersonsRecord personRecord :FactoryDAO.getPersonRecordDAO(em).getCDAsUserId(p.getId())){
+			
+						String query = "$doc//ClinicalDocument//id/@root='"+ personRecord.getPersonsRecordCdaRoot() + "' and " +
+						"$doc//ClinicalDocument//id/@extension='" + personRecord.getPersonsRecordCdaExtension() +"'";
+						orClausesCDAsIds.add(query);
 						
 					}
+					List<List<String>> or = new ArrayList<>();
+					or.add(orClauses);
+					try {
+						
+						if((andClauses.size() == 0 && orClauses.size() ==0) ||
+								(andClauses.size() != 0 && XMLDataBaseFactory.getIXMLDataBaseDonations().countQuery(andClauses,null,orClausesCDAsIds,null) > 0
+								&& XMLDataBaseFactory.getIXMLDataBaseDonations().countQuery(null,or,orClausesCDAsIds,null) == 0) || 
+								(andClauses.size() == 0 && XMLDataBaseFactory.getIXMLDataBaseDonations().countQuery(null,or,orClausesCDAsIds,null) == 0)){
+							
+							NotificationsPerson notificationPerson = new NotificationsPerson();
+							notificationPerson.setPerson(p);
+							notificationPerson.setNotification(notification);
+							notificationPerson.setSentOption(SentOptions.NOT_SENT);
+							persons.add(notificationPerson);
+							
+							/**
+							 * Enviar mail
+							 */
+							Message message = new MimeMessage(session);
+															
+							try {
+								
+								message.setFrom(new InternetAddress(username));
+								InternetAddress[] ret = new InternetAddress[1];
+								ret[0] = new InternetAddress(p.getPersonEmail());
+								
+								message.setRecipients(RecipientType.TO, ret);
+								message.setSubject(notification.getSubject());
+								
+								message.setContent(notification.getText(), "text/html");
+			
+								Transport.send(message);
+								notificationPerson.setSentOption(SentOptions.NOT_SENT);
+								
+							} catch (AddressException e) {
+								
+								logger.log(Level.SEVERE, "Error al enviar el mail AddressException", e);
+								
+								notificationPerson.setSentOption(SentOptions.ERROR);
+								
+							} catch (MessagingException e) {
+								
+								logger.log(Level.SEVERE, "Error al enviar el mail MessagingException", e);
+								notificationPerson.setSentOption(SentOptions.ERROR);
+							}
+							
+
+						}
+						
 					
 					
-				} catch (XMLDataBaseException e) {
-					
-					logger.log(Level.SEVERE, "Error al ir a buscar los cdas al baseX", e);
-					
+					} catch (XMLDataBaseException e) {
+						
+						logger.log(Level.SEVERE, "Error al ir a buscar los cdas al baseX", e);
+						
+					}
 				}
 	
 			}
