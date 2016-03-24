366984c5f467936c579d621b517fd264c6d329b0
paularoche <pula14@gmail.com>
2016-03-24 09:52:06 -0300

    tablas

--- Hemologica-Salud-Frontend/Hemologica-Salud-Frontend-web/src/main/webapp/resources/js/hemologica.js
+++ Hemologica-Salud-Frontend/Hemologica-Salud-Frontend-web/src/main/webapp/resources/js/hemologica.js
@@ -3,43 +3,53 @@
  */
 
 $(document).ready(function() {
-    $('#persontableDonation').DataTable({
-    	
-    	 'bSort': false,
-         'aoColumns': [
-               { sWidth: "15%", bSearchable: false, bSortable: false },
-               { sWidth: "15%", bSearchable: false, bSortable: false },
-               { sWidth: "15%", bSearchable: false, bSortable: false },
-               { sWidth: "15%", bSearchable: false, bSortable: false },
-               { sWidth: "15%", bSearchable: false, bSortable: false },
-               { sWidth: "15%", bSearchable: false, bSortable: false },
-               { sWidth: "15%", bSearchable: false, bSortable: false },
-               { sWidth: "15%", bSearchable: false, bSortable: false }
-         ],
-         "scrollY":        "200px",
-         "scrollCollapse": false,
-         "info":           false,
-         "paging":         true,
-         "searching":			false,
-         "bLengthChange" : false,
-         "pageLength": 50,
-         "retrieve": true,
-         
-         "language": {
-        	    "paginate": {
-        	      "first": "Primero",
-        	      "last": "Ultimo",
-        	      "next": "Siguiente",
-        	      "previous": "Anterior"
-        	    }
-        	  }
+	
+	persontableDonation();
+	persontableTransfusion();
+	persontable();
     
-    	}	
-    );
 } );
 
 
-$(document).ready(function() {
+
+function persontableDonation() {
+	
+	$('#persontableDonation').DataTable({
+		
+		 'bSort': false,
+	    'aoColumns': [
+	          { sWidth: "15%", bSearchable: false, bSortable: false },
+	          { sWidth: "15%", bSearchable: false, bSortable: false },
+	          { sWidth: "15%", bSearchable: false, bSortable: false },
+	          { sWidth: "15%", bSearchable: false, bSortable: false },
+	          { sWidth: "15%", bSearchable: false, bSortable: false },
+	          { sWidth: "15%", bSearchable: false, bSortable: false },
+	          { sWidth: "15%", bSearchable: false, bSortable: false },
+	          { sWidth: "15%", bSearchable: false, bSortable: false }
+	    ],
+	    "scrollY":        "200px",
+	    "scrollCollapse": false,
+	    "info":           false,
+	    "paging":         true,
+	    "searching":			false,
+	    "bLengthChange" : false,
+	    "pageLength": 50,
+	    "retrieve": true,
+	    
+	    "language": {
+	   	    "paginate": {
+	   	      "first": "Primero",
+	   	      "last": "Ultimo",
+	   	      "next": "Siguiente",
+	   	      "previous": "Anterior"
+	   	    }
+	   	  }
+	})
+}
+
+
+
+function  persontableTransfusion(){
     $('#persontableTransfusion').DataTable({
     	
     	 'bSort': false,
@@ -71,11 +81,11 @@ $(document).ready(function() {
         	    }
         	  }
     
-    }	
-    );
-} );
+    });
+}
 
-$(document).ready(function() {
+function persontable(){
+	
     $('#persontable').DataTable({
     	
     	 'bSort': false,
@@ -105,9 +115,8 @@ $(document).ready(function() {
         	    }
         	  }
     
-    }	
-    );
-} );
+    });
+}
 
 
 
